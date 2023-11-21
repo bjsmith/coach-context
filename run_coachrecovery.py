aa83d672170a9b129f641a3f0cf18bd33c65c47d
Ben J. Smith <benjsmith@gmail.com>
2023-11-21 09:08:50 -0800

    added some debug code

--- run_coachrecovery.py
+++ run_coachrecovery.py
@@ -1,6 +1,12 @@
 from telegram_connector_app import TelegramConnectorApp
 from chat import ChatConfig
-telegram_code = ChatConfig.get_config()['coachrecovery_bot']
+config = ChatConfig.get_config()
+telegram_code = config['coachrecovery_bot']
+
+print("start of chat config:")
+
+print(config['telegram_admin_ids'])
+print(len(config['telegram_admin_ids']))
 
 tca = TelegramConnectorApp(telegram_code)
 print("starting up connector app...")
